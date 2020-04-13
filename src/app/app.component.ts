1a71203f53d36c10b9e5c0f2d4b1eca053b09126
Kanav Arora <arorakanav11@gmail.com>
2020-04-13 16:20:34 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dataprogramming03Group13-webapp';
+}
